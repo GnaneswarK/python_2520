d41aac8a3ffd5c61830c9706c42bd9b75a8eddbc
Gnaneswar Korakuti <gnaneswarpandu1432@gmail.com>
2025-09-17 11:30:01 +0530

    string slicing type conversion

--- Basics/strings.py
+++ Basics/strings.py
@@ -60,4 +60,64 @@ print(len(element))
 print(element.count("o"))     # 2
 print(element.count("o", 0, 6))  # 1 (between index 0–6 only)
 print(element.count("z"))     # 0 (not found)
+# startswith    # returns boolean true or false
+print(element.strip().startswith("python"))
+# ends with     # returns boolean true or false
+print(element.strip().endswith("course"))
 
+#   STRING SLICING
+name = "prabhu ghajula"
+#   FOR SUBSTRING
+first_name = name[0:6]   # starts with 0 & ends with 6(doesnt take index 6)
+# this also same before colon if we didnt mention anything it starts from 0
+dup_first_name = name[:6]
+# starts with index 7 ends with index 14 doesnt take index 14
+last_name = name[7:14]
+# this also same if we dont mention anything after colon then its take len(name)
+dup_last_name = name[7:]
+full_name = name[0:14]  # stats with 0 ends with 14
+dup_full_name = name[:]  # this starts from start index to end index
+#   IF i WANT TO PRINT EVEN CHARECTERS FROM THE STRING THEN I WILL USE LIKE THI
+#   start:stop:step
+# here it is stats from zero index and ends with last index setp is 2 it prints like this pah hjl
+even_index = name[0:14:2]
+# see as i said before colom if i didnt mention then it starts from zero and after colon if i didnt mention its ends with len(name)
+#   above line i can mention like this also
+dup_even_index = name[::2]
+# similary for odd index
+# why starts with one beacuse odd number stats from 1 not zero
+odd_index = name[1::2]
+#   by default step is 1 so if you dont mention there nothing gonna happen so name[0:6] == name[0:6:1]
+# for fullname like this also we can do
+dup_full_name = name[::]  # this also starts from start index to end index
+#   if sept is -1 means here increment is by negative 1 which follows reverse order
+#   to print reverse of an string use like this [::-1] or [0:len(name):-1]
+reverse = name[::-1]
+
+
+#   PRINT STATEMNTS
+print("Original name: ", name)
+
+print("first_name:", first_name)
+print("dup_first_name:", dup_first_name)
+
+print("last_name:", last_name)
+print("dup_last_name:", dup_last_name)
+
+print("full_name:", full_name)
+print("dup_full_name:", dup_full_name)
+
+print("even_index:", even_index)
+print("dup_even_index:", dup_even_index)
+
+print("odd_index:", odd_index)
+
+print("reverse:", reverse)
+
+greeting = "Hello how are you?"
+my_list = greeting.split(" ")
+print(my_list)
+#   list to string
+# where in single code i use single space right that is delimeter to jion the list
+greeting = ' '.join(my_list)
+print(greeting)
--- Basics/typeConversion.py
+++ Basics/typeConversion.py
@@ -1,4 +1,6 @@
 import math
+from typing import Final
+
 x = input("x : ")
 # input comes from user is always string
 # y = x + 1  TypeError: can only concatenate str
@@ -13,10 +15,27 @@ print(f"after type casting {type(int(x))}")
 y = int(x) + 1
 print(f"value of y is {y}")
 
-### Falsy values are
+# Falsy values are
 #       ""
 #       0
 #       None
 
-#What are all the primitive data types in python?
+# What are all the primitive data types in python?
 #   -> numbers  -> strings  -> boolean
+
+
+marks = 25
+# int to string
+marks = str(marks)
+# string to int
+marks = int(marks)
+
+float_values = 3.765
+#   float to int    DATA LOSS WILL BE HAPPEN HERE WE WILL LOOSE 765 after decimal values
+float_to_int = int(float_values)
+
+#   int to float we dont loss any data
+float_values = float(float_to_int)
+
+
+
